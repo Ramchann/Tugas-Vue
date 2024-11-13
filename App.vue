c27038cd60e98a49eedcaab495a228449d8043b7
Ramdan // 拉姆丹 <134475978+Ramchann@users.noreply.github.com>
2024-11-13 13:41:51 +0700

    Update and rename vue.js to App.vue

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1 class="greeting">Selamat pagi tutor, ini adalah tugas kedua saya</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.greeting {
+  color: orange;
+  font-size: 24px;
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
